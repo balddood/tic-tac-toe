f27003301160e4f8bd5893270ba64fb0fb4ef1d4
Caleb McEntire <63273734+balddood@users.noreply.github.com>
2020-06-03 09:01:51 -0500

    Initial upload of script

---
+++ tic-tac-toe.py
@@ -0,0 +1,38 @@
+# Python 3 Bootcamp Milestone Project 1
+# Tic Tac Toe Game
+# Created 5/22/2020
+# By: Caleb McEntire
+
+# Making a board
+def board():
+    print('     |     |     ')
+    print('     |     |     ')
+    print('     |     |     ')
+    print('-----------------')
+    print('     |     |     ')
+    print('     |     |     ')
+    print('     |     |     ')
+    print('-----------------')
+    print('     |     |     ')
+    print('     |     |     ')
+    print('     |     |     ')
+
+def instructions():
+    print('Welcome to Tic-Tac-Toe!\n')
+    print('Instructions:')
+    print('This is a two player game. Each player will alternate turns.')
+    print('In order to place your marker on the board type a number on the number')
+    print('pad of your keyboard that corresponds to a position on the board below.')
+    
+    print('     |     |     ')
+    print('  7  |  8  |  9  ')
+    print('     |     |     ')
+    print('-----------------')
+    print('     |     |     ')
+    print('  4  |  5  |  6  ')
+    print('     |     |     ')
+    print('-----------------')
+    print('     |     |     ')
+    print('  1  |  2  |  3  ')
+    print('     |     |     ')
+instructions()     
